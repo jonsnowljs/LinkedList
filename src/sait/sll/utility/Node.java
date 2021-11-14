8662a476596153a7904b8bcc155c70b9cae916fc
Jiasheng Lu <jonsnowljs@gmail.com>
2021-11-14 14:02:55 -0700

    create structure of Node and SLL class

---
+++ src/sait/sll/utility/Node.java
@@ -0,0 +1,14 @@
+package sait.sll.utility;
+
+public class Node {
+	String info;
+	Node nextNode;
+	
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+}
---
+++ src/sait/sll/utility/SLL.java
@@ -0,0 +1,71 @@
+package sait.sll.utility;
+
+public class SLL implements LinkedListADT   {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void append(Object data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void prepend(Object data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insert(Object data, int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void replace(Object data, int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delete(int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object retrieve(int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object data) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean contains(Object data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
